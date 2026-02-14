ec484f01235673bdc60183d1c22c2b71a1a8a403
guka199 <gukajanashia1@gmail.com>
2026-02-14 00:52:30 -0500

    Fix static files, Procfile, and key access

--- puzzle/views.py
+++ puzzle/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponse
 
 def index(request):
+    if request.GET.get("key") != "abc123":
+        return HttpResponse("Not allowed")
     return render(request, "puzzle/index.html")
+
